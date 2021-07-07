e30d8866f1508eaabe5cea354f79d7acc063d887
leodeorce <42984982+leodeorce@users.noreply.github.com>
2021-07-07 12:25:54 -0300

    Ordenações variando quantidade inicial de threads

--- Main.java
+++ Main.java
@@ -5,38 +5,50 @@ import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         final int TAMANHO_VETOR = 50_000_000;
-        int[] vetor = new int[TAMANHO_VETOR];
+        int[] vetorReferencia = new int[TAMANHO_VETOR];
         // Gera valores aleatorios
-        for(int i = 0; i < vetor.length; i++) {
-                // Valores no intervalo [ 0; 2*TAMANHO-VETOR ]
+        for(int i = 0; i < vetorReferencia.length; i++) {
+            // Valores no intervalo [ 0; 2*TAMANHO-VETOR ]
             int numero = (int) Math.round(Math.random() * 2 * TAMANHO_VETOR);
-            vetor[i] = numero;
+            vetorReferencia[i] = numero;
         }
+//        System.out.println(Arrays.toString(vetorReferencia));
         int[] k = {8, 4, 2, 1};
         ArrayList<Thread> threads = new ArrayList<>();
         // Executa k threads por vez
-        for(int numThreads : k) {
-            // Porcentagem do vetor que uma thread deve ordenar
-            double fracao = 1.0 / numThreads;
-            for (int j = 0; j < numThreads; j++) {
-                // (j * fracao) e' o percentil que indica onde essa thread deve comecar
-                int inicio = (int) Math.round(Math.floor(j * fracao * TAMANHO_VETOR));
-                // ((j + 1) * fracao) e' o percentil que indica onde essa thread deve terminar
-                int fim = (int) (inicio + fracao * TAMANHO_VETOR);
-                Ordena ordena = new Ordena(vetor, inicio, fim);
-                Thread thread = new Thread(ordena);
-                threads.add(thread);
-                thread.start();
-            }
-            for(Thread thread : threads) {
-                try {
-                    thread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        for(int numThreadsInicial : k) {
+            int[] vetor = vetorReferencia;
+            double numThreads = numThreadsInicial;
+            System.out.println("k inicial : " + numThreadsInicial + " {");
+            while(numThreads >= 1) {
+                // Porcentagem do vetor que uma thread deve ordenar
+                double fracao = 1.0 / numThreads;
+                System.out.println("\tk efetivo : " + numThreads + " {");
+                for (int j = 0; j < numThreads; j++) {
+                    // (j * fracao) e' o percentil que indica onde essa thread deve comecar
+                    int inicio = (int) Math.round(Math.floor(j * fracao * TAMANHO_VETOR));
+                    // (inicio * fracao) e' o percentil que indica onde essa thread deve terminar
+                    int fim = (int) (inicio + fracao * TAMANHO_VETOR);
+                    Ordena ordena = new Ordena(vetor, inicio, fim);
+                    Thread thread = new Thread(ordena);
+                    threads.add(thread);
+                    thread.start();
+                    System.out.println("\t\tinicio : " + inicio);
+                    System.out.println("\t\tfim : " + fim);
                 }
+                for (Thread thread : threads) {
+                    try {
+                        thread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                threads.clear();
+                numThreads /= 2;
+                System.out.println("\t}");
             }
-            threads.clear();
+            System.out.println("}");
+//            System.out.println(Arrays.toString(vetor));
         }
-        System.out.println(Arrays.toString(vetor));
     }
 }
